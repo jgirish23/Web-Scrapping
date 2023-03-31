a903e84e817b5345a4e73c76196cf9757bb5b329
jgirish23 <jakkalagirish@gmail.com>
2023-03-31 17:28:35 +0530

    v1

---
+++ scrapper.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import csv
+from datetime import date
+
+html=requests.get("http://theverge.com")
+html=html.text
+soup= BeautifulSoup(html,"html.parser")
+
+today=date.today()
+d=today.strftime("%d%m%y")
+
+def main():
+    print("scrapping started!")
+    def obj(Id,Headline,Url,Author,Date):
+        return [Id,Headline,Url,Author,Date]
+
+    id=list()
+    headline=list()
+    author=list()
+    curr_date=list()
+    url=list()
+
+    #id
+    for ids in soup.find_all(class_="z-10 flex h-5 w-5 items-center justify-center rounded-full bg-gray-31/90 font-polysans text-11 text-franklin"):
+        id.append(ids.contents[0])
+
+    # headline and url
+    for i in soup.find_all(class_="group-hover:shadow-underline-franklin",href=True):
+        url.append("https://www.theverge.com"+i['href'])
+        headline.append(i.contents[0])
+
+    # author
+    for i in soup.find_all(class_="text-gray-31 hover:shadow-underline-inherit dark:text-franklin mr-8"):
+        author.append(i.contents[0])
+
+    # date
+    for i in soup.find_all(class_="text-gray-63 dark:text-gray-94"):
+        curr_date.append(i.contents[0])
+
+
+    rows=["Id", "Headline", "Url", "Author", "Date"]
+    store=list()
+    for i in range(len(id)):
+        data=obj(id[i],headline[i],url[i],author[i],d+" " +curr_date[i])
+        store.append(data)
+
+    # print(store)
+    print(d)
+    with open(f"./JSON_Data/{d}.json",'w') as file:
+        json.dump(store,file,indent=4)
+
+    with open(f"./ScrappedData/{d}_verge", 'w') as file:
+        write = csv.writer(file,delimiter=",")
+        write.writerow(rows)
+        write.writerows(store)
+
+
+if __name__=='__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,73 @@
+from flask import Flask, jsonify
+import sqlite3 as sql
+app = Flask(__name__)
+import scrapper
+import os
+import json
+from datetime import date
+dt=date.today().strftime("%d%m%y")
+
+
+con = sql.connect('database.db',check_same_thread=False)
+con.execute("CREATE TABLE IF NOT EXISTS articles(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT,link TEXT,author TEXT type UNIQUE,datenow TEXT);")
+con.close()
+
+
+def get_db_connection():
+    conn = sql.connect('database.db')
+    conn.row_factory = sql.Row
+    return conn
+
+def Background_Script():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_data = os.path.join(SITE_ROOT, "./JSON_Data/", f"{dt}_verge.json")
+    data = json.load(open(json_data))
+    print("Data extracted")
+    try:
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            for item in data:
+                id = int(item[0])
+                title = item[1]
+                url = item[2]
+                author = item[3]
+                date = item[4]
+                cur.execute("INSERT INTO articles(title,link,author,datenow) VALUES (?,?,?,?)",(title, url, author, date))
+                con.commit()
+            msg = "Record successfully added"
+            print(msg)
+    except:
+        con.rollback()
+        msg = "error in insert operation"
+        print(msg)
+    
+    finally:
+        con.close()
+
+def Current_Day():
+    scrapper.main()
+    Background_Script()
+
+@app.route('/')
+def home():
+    Current_Day()
+    conn = get_db_connection()
+    data = conn.execute('SELECT * FROM articles').fetchall()
+    # d=conn.execute('SELECT * FROM articles WHERE id=? ', '2').fetchall()
+    # print(len(d))
+    conn.close()
+    _json=[]
+    key=["id","title","link","author","datenow"]
+    for i in range(len(data)):
+        item={}
+        ct=0
+        for j in data[i]:
+            item[key[ct]]=j
+            ct+=1
+        _json.append(item)
+    print(_json)
+    return jsonify(_json)
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
